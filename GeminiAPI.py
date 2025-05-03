2d302e7406b225b10a87a22eca06f0235efc4789
Ludwin Tenezaca <ludwin.tenezaca38@myhunter.cuny.edu>
2025-05-03 16:46:37 -0400

    made repo ready for backend only deployment and testing

--- GeminiAPI.py
+++ GeminiAPI.py
@@ -1,9 +1,12 @@
 from google import genai
 from google.genai import types
-import privconfig as p
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def get_gemini_suggestions(budget_info):
-    client = genai.Client(api_key=p.API_Key)
+    client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
     price = str(budget_info["budget"])
     area = budget_info["category"]
@@ -11,9 +14,10 @@ def get_gemini_suggestions(budget_info):
     response = client.models.generate_content(
         model="gemini-2.0-flash",
         config=types.GenerateContentConfig(
-            system_instruction="You are a financial advisor..."
+            system_instruction="You are an expert travel, lifestyle assistant and financial advisor. Given a user's budget and selected category (e.g., travel, food experiences, fitness, education), generate 3 realistic, achievable options they can do. Each option must be 1 to 2 sentences, concise, and have an example with a real places, foods, activities, or services that the user could actually look up online. Make the suggestions feel exciting, personal, and within the given budget. Return only the list — no intro, no explanations, no numbering. Format the output as a list of strings separated by '`'. Example of a suggestion: '3 - 4 Day City Escape: Explore Lisbon, Portugal or Mexico City. Enjoy affordable flights, boutique hotels, and authentic local food — all within budget.'"
         ),
         contents=f"What are three ways I can spend a budget of {price} on {area}, in a way that will stay within my means of living?"
+
     )
 
     return response.text
--- connectAPI.py
+++ connectAPI.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from userInputRunner import run_pipeline
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/get-suggestions", methods=["POST"])
 def get_suggestions():
