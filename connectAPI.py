d25c8a5d7c791dbd66ec0aa98d23f6c1e37a4274
Mohameda9017 <mohameda9047@gmail.com>
2025-05-03 21:40:58 -0400

    ready to deploy

--- connectAPI.py
+++ connectAPI.py
@@ -5,14 +5,21 @@ from userInputRunner import run_pipeline
 app = Flask(__name__)
 CORS(app)
 
+@app.route("/")
+def home():
+    return "WE LIVE BABY"
+
 @app.route("/get-suggestions", methods=["POST"])
 def get_suggestions():
     data = request.json
     budget = data.get("budget")
     category = data.get("category")
 
-    result = run_pipeline(budget, category)
-    return jsonify(result)
+    try:
+        result = run_pipeline(budget, category)
+        return jsonify(result)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
